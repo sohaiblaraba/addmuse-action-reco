50159b251eac7048de825a48fb8af6745e2d355b
sohaiblaraba <sohaib-l@hotmail.fr>
2022-05-09 12:31:51 +0200

    minor update in testsender

--- testsend.py
+++ testsend.py
@@ -1,24 +1,32 @@
 from communication import *
 import cv2
 import numpy as np
+import time
 
 com = Communication(myip='localhost', receiveport=8100, sendip ='localhost', sendport=8000)
 com.start()
 
 
 black = np.zeros((256, 256))
+t1 = t2 = 0
 while True:
     cv2.imshow('', black)
     k = cv2.waitKey(10)
     if k == ord('q'):
         break
     if k == ord('s'):
-        com.send('/start', 0)
+        com.custom_send('/start', 0)
+        print('SENDING:', '/start 0')
+        cv2.putText(black, 'Sending: /start 0', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), thickness=1)
+        t1 = time.time()
     if k == ord('p'):
-        com.send('/stop', 0)
+        com.custom_send('/stop', 0)
+        print('SENDING:', '/stop 0')
+        cv2.putText(black, 'Sending: /stop 0', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), thickness=1)
+        t1 = time.time()
 
+    t2 = time.time()
 
+    if t2 - t1 >= 1 and t2-t1 < 1.5:
+        black = np.zeros((256, 256))
 
-
-
-# com.send('BLABLA')
